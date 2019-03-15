a40f759536bfb5572f17497819b53f2fb531607b
unknown <antoniotx.dev@gmail.com>
2019-03-15 19:51:22 -0300

    Adiciona sourcemaps e react hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 
 // Objeto que o webpack precisa pra fazer a leitura do nosso arquivo  gerar o bundle
 module.exports = {
+    // Serve para mostrar o erro no console do navegador direto no arquivo, ao inves do bundle
+    devtool: 'source-map',
     // O path join junta o caminho para servir em diversos sistemas operacionais
     entry: path.join(__dirname, 'src', 'index'),
     // Onde o webpack vai salvar
