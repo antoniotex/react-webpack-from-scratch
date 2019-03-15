97ec83b798f5f2793c7f81b5316c10098fdb8373
unknown <antoniotx.dev@gmail.com>
2019-03-15 19:39:21 -0300

    Adiciona packages do babel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Title = require('./app')
+import React from'react';
+import ReactDOM from 'react-dom';
+import Title from './app'
 
 ReactDOM.render(
     React.createElement(Title),
--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,13 @@ module.exports = {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/static/'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            include: /src/,
+            loader: 'babel'
+        }]
     }
 }
