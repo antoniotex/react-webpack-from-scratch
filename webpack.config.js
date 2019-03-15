3976be937dcd2da9a520eeae08274fbec0a29ae6
unknown <antoniotx.dev@gmail.com>
2019-03-15 20:28:59 -0300

    Primeira config do hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,27 @@
 const path = require('path')
+const webpack = require('webpack')
 
 // Objeto que o webpack precisa pra fazer a leitura do nosso arquivo  gerar o bundle
 module.exports = {
     // Serve para mostrar o erro no console do navegador direto no arquivo, ao inves do bundle
     devtool: 'source-map',
     // O path join junta o caminho para servir em diversos sistemas operacionais
-    entry: path.join(__dirname, 'src', 'index'),
+    entry: [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        path.join(__dirname, 'src', 'index')
+    ],
     // Onde o webpack vai salvar
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/static/'
     },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
         loaders: [{
             test: /\.js$/,
