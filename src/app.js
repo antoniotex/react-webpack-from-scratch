532038763204bd12f26a7f5a66f113c125cf04ca
unknown <antoniotx.dev@gmail.com>
2019-03-15 22:57:10 -0300

    Adiciona configurações de lint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-const  App = React.createClass({
-    render: function () {
-        return <div>Aplicação</div>
-    }
+const App = React.createClass({
+  render: function () {
+    return <div>Aplicação</div>
+  }
 })
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import App from './app'
 import { AppContainer } from 'react-hot-loader'
 
 ReactDOM.render(
-    <AppContainer>
-        <App />
-    </AppContainer>,
-    document.querySelector('[data-js="app"]')
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  document.querySelector('[data-js="app"]')
 )
 
-if(module.hot){
-    module.hot.accept('./app', () => {
-        const NextApp = require('./app').default
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    const NextApp = require('./app').default
 
-        ReactDOM.render(
-            <AppContainer>
-                <NextApp />
-            </AppContainer>,
-            document.querySelector('[data-js="app"]')
-        )
-    })
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp />
+      </AppContainer>,
+      document.querySelector('[data-js="app"]')
+    )
+  })
 }
